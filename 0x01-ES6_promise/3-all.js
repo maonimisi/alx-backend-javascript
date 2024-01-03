d884839bc9c13250a5584dceeb68b02c3a9a1b36
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-01-03 13:08:40 +0100

    All

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,8 +6,8 @@ import {
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((response) => {
-      const body = response.shift();
-      const [firstName, lastName] = response.pop(); // Corrected the syntax here
+      const {body} = response.shift();
+      const {firstName, lastName} = response.pop();
       console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
