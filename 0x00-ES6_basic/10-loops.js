f736240facaf62fdb8add20cc459f178411116a8
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-01-03 07:31:07 +0100

    Loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
 
-  for (const idx of array) {
-   newArray.push(appendString + idx);
+  for (const el of array) {
+    newArray.push(appendString + el);
   }
 
   return newArray;
