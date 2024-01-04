5664b5d5dd0ab9a83c008db433990a41d0f34dae
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-01-04 21:25:29 +0100

    Classes

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](atr) {
+    if (atr === 'number') {
+      return `${this._size}`
+
+    }
+    return `${this._location}`;
+  }
+}
+
+export default HolbertonClass;
