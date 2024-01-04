748c6c1700214bc1545f86c0063fd8ee60b6cfdf
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-01-04 15:14:02 +0100

    Classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  const new_array = [];
-  new_array.push(new ClassRoom(19));
-  new_array.push(new ClassRoom(20));
-  new_array.push(new ClassRoom(34));
+  const newArray = [];
+  newArray.push(new ClassRoom(19));
+  newArray.push(new ClassRoom(20));
+  newArray.push(new ClassRoom(34));
 
-  return new_array;
+  return newArray;
 }
