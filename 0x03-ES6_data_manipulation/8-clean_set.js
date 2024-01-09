ec040cf3c9bd0dca770204f9218d1f6a27f4f6cc
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-01-09 07:34:49 +0100

    Data Manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,8 +4,10 @@ export default function cleanSet(set, startString) {
     return '';
   }
   for (const elem of set) {
-    if ((typeof elem === 'string') && (elem.startsWith(startString)))
+    if ((typeof elem === 'string') && (elem.startsWith(startString))) {
       newList.push(elem.slice(startString.length));
+    }
   }
   return newList.join('-');
 }
+
