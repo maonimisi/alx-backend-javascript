6521bf4f1dc368502c986fdae8666b073fda4b46
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-01-09 07:17:26 +0100

    Data Manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,5 @@ export default function getListStudentId(listStudent) {
   if (!Array.isArray(listStudent)) {
     return [];
   }
- return listStudent.map((obj) => obj.id);
+  return listStudent.map((obj) => obj.id);
 }
