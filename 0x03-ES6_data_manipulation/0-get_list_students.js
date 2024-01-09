3da1539391f225095d078d56f0ffa56d5729d1d4
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-01-09 07:15:23 +0100

    Data Manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,7 +2,8 @@ export default function getListStudents() {
   const listStudents = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+
   return listStudents;
 }
